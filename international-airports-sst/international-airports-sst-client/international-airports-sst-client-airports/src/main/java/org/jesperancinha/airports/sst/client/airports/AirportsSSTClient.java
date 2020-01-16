1e788549e32e5d391a5273dbe72e9f8ea7b270cb
Joao Esperancinha <jofisaes@gmail.com>
2020-01-17 00:40:06 +0100

    Search word is now coupled

--- international-airports-sst/international-airports-sst-client/international-airports-sst-client-airports/src/main/java/org/jesperancinha/airports/sst/client/airports/AirportsSSTClient.java
+++ international-airports-sst/international-airports-sst-client/international-airports-sst-client-airports/src/main/java/org/jesperancinha/airports/sst/client/airports/AirportsSSTClient.java
@@ -1,6 +1,7 @@
 package org.jesperancinha.airports.sst.client.airports;
 
 import com.google.gson.Gson;
+import com.squareup.okhttp.HttpUrl;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -10,23 +11,42 @@ import org.jesperancinha.airports.sst.client.airports.model.Airport;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 @Builder
 @AllArgsConstructor
 public class AirportsSSTClient extends OkHttpClient {
 
-    private final URL url;
+    private final String url;
 
-    private final Request request;
+    private final String xRapidAPIHost;
+
+    private final String xRapidAPIKey;
 
     @Builder.Default
     private final Gson gson = new Gson();
 
     public Flux<Airport> findAllAiportsBySearchWord(final String searchWord) {
         return Mono.fromCallable(() -> {
-            Response response = this.newCall(request).execute();
+            final Response response = this.newCall(getBuild(searchWord)).execute();
             return gson.fromJson(response.body().string(), Airport[].class);
         }).flatMapMany(Flux::fromArray);
+    }
+
+    private Request getBuild(String append) throws MalformedURLException {
+        return new Request.Builder()
+                .url(getFullQueryTextUrl(append))
+                .get()
+                .addHeader("x-rapidapi-host", xRapidAPIHost)
+                .addHeader("x-rapidapi-key", xRapidAPIKey)
+                .build();
+    }
+
+    private URL getFullQueryTextUrl(String append) {
+        return HttpUrl.parse(url)
+                .newBuilder()
+                .addEncodedQueryParameter("text", append)
+                .build().url();
     }
 }
--- international-airports-sst/international-airports-sst-client/international-airports-sst-client-airports/src/main/java/org/jesperancinha/airports/sst/client/airports/config/AirportsSSTClientConfiguration.java
+++ international-airports-sst/international-airports-sst-client/international-airports-sst-client-airports/src/main/java/org/jesperancinha/airports/sst/client/airports/config/AirportsSSTClientConfiguration.java
@@ -1,33 +1,22 @@
 package org.jesperancinha.airports.sst.client.airports.config;
 
-import com.squareup.okhttp.Request;
 import org.jesperancinha.airports.sst.client.airports.AirportsSSTClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.net.URL;
 
 @Configuration
 public class AirportsSSTClientConfiguration {
 
     @Bean
     public AirportsSSTClient webClient(
-            @Value("${org.jesperancinha.airports.sst.client.airports.url}") URL url,
+            @Value("${org.jesperancinha.airports.sst.client.airports.url}") String url,
             @Value("${org.jesperancinha.airports.sst.client.airports.x.rapidapi.host}") String xRapidAPIHost,
             @Value("${org.jesperancinha.airports.sst.client.airports.x.rapidapi.key}") String xRapidAPIKey) {
-        return AirportsSSTClient.builder().url(url)
-                .request(getBuild(xRapidAPIHost, xRapidAPIKey))
+        return AirportsSSTClient.builder()
+                .url(url)
+                .xRapidAPIHost(xRapidAPIHost)
+                .xRapidAPIKey(xRapidAPIKey)
                 .build();
     }
-
-    private Request getBuild(String xRapidAPIHost, String xRapidAPIKey) {
-        return new Request.Builder()
-                .url("https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-text?text=Be")
-                .get()
-                .addHeader("x-rapidapi-host", xRapidAPIHost)
-                .addHeader("x-rapidapi-key", xRapidAPIKey)
-                .build();
-    }
-
 }
