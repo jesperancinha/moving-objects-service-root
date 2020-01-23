1fed3f62147c90862b4dc6743e6c71755760d478
Joao Esperancinha <jofisaes@gmail.com>
2020-01-23 01:37:50 +0100

    Complete build script

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+gradle build install -p international-airports-sst/international-airports-sst-client/international-airports-sst-client-webcam
+gradle build install -p international-airports-sst/international-airports-sst-client/international-airports-sst-client-airports
+gradle build install -p international-airports-sst/international-airports-sst-data
+gradle build install -p international-airports-sst/international-airports-sst-mock
+gradle build install -p international-airports-sst/international-airports-sst-live
+gradle build install -p international-airports-rest/international-airports-data
+gradle build install -p international-airports-rest/international-airports-model
+gradle build install -p international-airports-rest/international-airports-service-api
+gradle build install -p international-airports-rest/international-airports-rest-api
+gradle build install -p international-airports-rest/international-airports-rest-service
+
 docker-machine start dev
 
 docker-machine env dev
