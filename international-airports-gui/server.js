0c01b7b70b5a521a1a4c7084b5968c1d8b3a3624
Joao Esperancinha <jofisaes@gmail.com>
2022-01-16 16:15:07 +0100

    🏞 Migration to JDK 17

--- international-airports-gui/server.js
+++ international-airports-gui/server.js
@@ -25,13 +25,13 @@ var Server = /** @class */ (function () {
         this.app.use(apiMetrics());
         this.app.get('*', function (req, res) {
             if (allowedExt.filter(function (ext) { return req.url.indexOf(ext) > 0; }).length > 0) {
-                res.sendFile(path.resolve("dist/" + req.url));
+                res.sendFile(path.resolve("dist/".concat(req.url)));
             }
             else {
                 res.sendFile(path.resolve('dist/index.html'));
             }
         });
-        this.app.listen(this.port, function () { return console.log("Express http is started " + _this.port); });
+        this.app.listen(this.port, function () { return console.log("Express http is started ".concat(_this.port)); });
         this.app.on('error', function (error) {
             console.error(moment().format(), 'An error has ocurred!', error);
         });
