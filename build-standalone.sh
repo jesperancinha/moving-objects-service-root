d69e3e36104af1f1a5ddb81a121df45a3325cb3b
Joao Esperancinha <jofisaes@gmail.com>
2022-07-18 17:33:36 +0200

    Improvements

--- build-standalone.sh
+++ build-standalone.sh
@@ -6,14 +6,14 @@ docker rm app-ia
 
 gradle clean build install test
 
-cd international-airports-gui
+cd international-airports-gui || exit
 yarn install --force
 yarn build
 cd ..
 
 echo "Building with --build-arg AIRPORTS_KEY=$1 --build-arg WEBCAMS_KEY=$2 -t app-ia-image"
 
-docker build . --build-arg AIRPORTS_KEY=$1 --build-arg WEBCAMS_KEY=$2 -t app-ia-image
+docker build . --build-arg AIRPORTS_KEY="$1" --build-arg WEBCAMS_KEY="$2" -t app-ia-image
 
 echo "Let's just open 8080 and 8082 and 4000. This way we can see that the single source of truth service is running, but not accessible from the outside."
 docker run --name app-ia -d -p 8080:80 -p 8082:8082 -p 4000:4000 app-ia-image
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-java -jar international-airports-sst-live-1.0.0.jar --rapidapi.airports.key=$1 --rapidapi.webcams.key=$2 &
+java -jar international-airports-sst-live-1.0.0.jar --rapidapi.airports.key="$1" --rapidapi.webcams.key="$2" &
 java -jar international-airports-rest-service-1.0.0.jar
