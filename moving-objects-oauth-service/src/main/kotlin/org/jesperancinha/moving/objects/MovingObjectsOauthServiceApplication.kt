fdaee183fbcbee8ef5a4a1302694cd1c2e5c4956
Joao Esperancinha <jofisaes@gmail.com>
2022-07-23 19:43:26 +0100

    R2dbc settings

--- moving-objects-oauth-service/src/main/kotlin/org/jesperancinha/moving/objects/MovingObjectsOauthServiceApplication.kt
+++ moving-objects-oauth-service/src/main/kotlin/org/jesperancinha/moving/objects/MovingObjectsOauthServiceApplication.kt
@@ -2,8 +2,10 @@ package org.jesperancinha.moving.objects
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 class MovingObjectsOauthServiceApplication
 
 fun main(args: Array<String>) {
