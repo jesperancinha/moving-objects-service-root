ce4095525f2abaf4c08d5e2fd830f7df6fd004d0
Joao Esperancinha <jofisaes@gmail.com>
2022-07-23 21:43:03 +0100

    R2dbc driver

--- moving-objects-oauth-service/build.gradle.kts
+++ moving-objects-oauth-service/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("io.r2dbc:r2dbc-postgresql:0.8.12.RELEASE")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
