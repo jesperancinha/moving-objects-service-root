0949857f6ad03892f261adf907fc03a1e6520300
Joao Esperancinha <jofisaes@gmail.com>
2022-07-23 19:27:37 +0100

    mos db test

--- moving-objects-oauth-service/entrypoint.sh
+++ moving-objects-oauth-service/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -jar -Dpostgres.host=${MOS_DB_IP} --spring.profiles.active=docker moving-objects-oauth-service.jar
+java -jar -Dpostgres.host="${MOS_DB_IP}" moving-objects-oauth-service.jar --spring.profiles.active=docker
