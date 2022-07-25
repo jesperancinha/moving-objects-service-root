9e30b4ec9bf4305c4005ef2e56de494a1599bab4
João Filipe Sabino Esperancinha <jofisaes@hotmail.com>
2022-07-25 16:25:35 +0000

    Update plugin org.jetbrains.kotlin.jvm to v1.7.10

--- moving-objects-oauth-service/build.gradle.kts
+++ moving-objects-oauth-service/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.1"
     id("io.spring.dependency-management") version "1.0.12.RELEASE"
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.10"
     kotlin("plugin.spring") version "1.6.21"
     id("jacoco")
     id("maven-publish")
