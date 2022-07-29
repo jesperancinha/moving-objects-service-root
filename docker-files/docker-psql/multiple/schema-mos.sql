7ff6ad22f5797254d25bd15f123f1f9f5ebbbabd
Joao Esperancinha <jofisaes@gmail.com>
2022-07-29 20:43:24 +0200

    Upgrade SQL Schema

--- docker-files/docker-psql/multiple/schema-mos.sql
+++ docker-files/docker-psql/multiple/schema-mos.sql
@@ -1,1 +1,19 @@
 create schema if not exists mos;
+
+create table if not exists mos.moving_object(
+    id UUID,
+    "name" VARCHAR(50) UNIQUE NOT NULL,
+    code VARCHAR ( 50 ) NOT NULL,
+    folder VARCHAR ( 255 ) UNIQUE NOT NULL,
+    url VARCHAR(255 ) UNIQUE NOT NULL,
+    x INT,
+    y INT,
+    PRIMARY KEY (id)
+);
+
+create table if not exists mos.info_object(
+    id UUID,
+    "size" INT,
+    color VARCHAR ( 255 ) UNIQUE NOT NULL,
+    PRIMARY KEY (id)
+);
