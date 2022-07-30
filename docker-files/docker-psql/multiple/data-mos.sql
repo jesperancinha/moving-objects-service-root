79e93ace681b76d5423f8e6a827738c7f540fcf3
Joao Esperancinha <jofisaes@gmail.com>
2022-07-30 07:15:20 +0200

    Connects to schema

--- docker-files/docker-psql/multiple/data-mos.sql
+++ docker-files/docker-psql/multiple/data-mos.sql
@@ -1,18 +1,18 @@
 truncate mos.mos.moving_object;
 
-insert into mos.mos.moving_object (id, "name", code, folder, url)
-values (gen_random_uuid(), 'garlic', 'GAR', 'garlic', 'http://moving-objects-jwt-service:8080/objects/camera/gar');
-insert into mos.mos.moving_object (id, "name", code, folder, url)
-values (gen_random_uuid(), 'laurel', 'LAU', 'laurel', 'http://moving-objects-jwt-service:8080/objects/camera/lau');
-insert into mos.mos.moving_object (id, "name", code, folder, url)
-values (gen_random_uuid(), 'lemon', 'LEM', 'lemon', 'http://moving-objects-jwt-service:8080/objects/camera/lem');
-insert into mos.mos.moving_object (id, "name", code, folder, url)
-values (gen_random_uuid(), 'onion', 'ONI', 'onion', 'http://moving-objects-jwt-service:8080/objects/camera/oni');
-insert into mos.mos.moving_object (id, "name", code, folder, url)
-values (gen_random_uuid(), 'pumpkin', 'PUM', 'pumpkin', 'http://moving-objects-jwt-service:8080/objects/camera/pum');
-insert into mos.mos.moving_object (id, "name", code, folder, url)
-values (gen_random_uuid(), 'red-onion', 'RED', 'red-onion', 'http://moving-objects-jwt-service:8080/objects/camera/red');
-insert into mos.mos.moving_object (id, "name", code, folder, url)
-values (gen_random_uuid(), 'snail', 'SNA', 'snail', 'http://moving-objects-jwt-service:8080/objects/camera/sna');
-insert into mos.mos.moving_object (id, "name", code, folder, url)
-values (gen_random_uuid(), 'tomato', 'TOM', 'tomato', 'http://moving-objects-jwt-service:8080/objects/camera/tom');
+insert into mos.mos.moving_object (id, "name", code, folder, url,x,y)
+values (gen_random_uuid(), 'garlic', 'GAR', 'garlic', 'http://moving-objects-jwt-service:8080/objects/camera/gar',1,2);
+insert into mos.mos.moving_object (id, "name", code, folder, url,x,y)
+values (gen_random_uuid(), 'laurel', 'LAU', 'laurel', 'http://moving-objects-jwt-service:8080/objects/camera/lau',2,2);
+insert into mos.mos.moving_object (id, "name", code, folder, url,x,y)
+values (gen_random_uuid(), 'lemon', 'LEM', 'lemon', 'http://moving-objects-jwt-service:8080/objects/camera/lem',3,4);
+insert into mos.mos.moving_object (id, "name", code, folder, url,x,y)
+values (gen_random_uuid(), 'onion', 'ONI', 'onion', 'http://moving-objects-jwt-service:8080/objects/camera/oni',7,2);
+insert into mos.mos.moving_object (id, "name", code, folder, url,x,y)
+values (gen_random_uuid(), 'pumpkin', 'PUM', 'pumpkin', 'http://moving-objects-jwt-service:8080/objects/camera/pum',8,1);
+insert into mos.mos.moving_object (id, "name", code, folder, url,x,y)
+values (gen_random_uuid(), 'red-onion', 'RED', 'red-onion', 'http://moving-objects-jwt-service:8080/objects/camera/red',3,6);
+insert into mos.mos.moving_object (id, "name", code, folder, url,x,y)
+values (gen_random_uuid(), 'snail', 'SNA', 'snail', 'http://moving-objects-jwt-service:8080/objects/camera/sna',1,7);
+insert into mos.mos.moving_object (id, "name", code, folder, url,x,y)
+values (gen_random_uuid(), 'tomato', 'TOM', 'tomato', 'http://moving-objects-jwt-service:8080/objects/camera/tom',0,1);
