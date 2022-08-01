43c291489f1d433ba022095674c3111f74b7c5d7
Joao Esperancinha <jofisaes@gmail.com>
2022-08-01 16:29:53 +0200

    Airports to Objects

--- moving-objects-gui/src/app/model/airport.spec.ts
+++
@@ -1,6 +0,0 @@
-import {MovingObject} from './movingObject';
-
-test('should create an object', () => {
-    const airport = new MovingObject();
-    expect(airport).not.toBeNull();
-});
---
+++ moving-objects-gui/src/app/model/objects.spec.ts
@@ -0,0 +1,6 @@
+import {MovingObject} from "./movingObject";
+
+test("should create an object", () => {
+    const airport = new MovingObject();
+    expect(airport).not.toBeNull();
+});
