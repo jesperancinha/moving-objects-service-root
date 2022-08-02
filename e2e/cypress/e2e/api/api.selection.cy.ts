7d9a1edaf72bc3be4d5d95c029f327ae283b88dc
Joao Esperancinha <jofisaes@gmail.com>
2022-08-02 12:59:05 +0200

    Extending cypress

--- e2e/cypress/e2e/api/api.selection.cy.ts
+++ e2e/cypress/e2e/api/api.selection.cy.ts
@@ -6,12 +6,15 @@ describe('API Images test ', () => {
         cy.visit(`http://${host}:${port}`);
         cy.get('div > div > div > input').should('exist');
         cy.get('div > div > div > input').should('have.value', 0);
-        const secondFormField = cy.get('app-webcams-selector > div > div > mat-form-field').eq(1)
+        const secondFormField = cy.get('app-webcams-selector > div > div > mat-form-field').eq(1);
         secondFormField.should('exist');
-        const filterInputField = secondFormField.get('div > div > div > input').eq(1)
+        const filterInputField = secondFormField.get('div > div > div > input').eq(1);
         filterInputField.should('exist');
         filterInputField.should('not.have.value', 0);
         filterInputField.should('have.value', '');
         filterInputField.type("Garlic",{force: true});
+        cy.get('span[class="mat-option-text"]').contains('Garlic').click();
+        cy.get('span[class="mat-button-wrapper"]').click();
+
     });
 })
