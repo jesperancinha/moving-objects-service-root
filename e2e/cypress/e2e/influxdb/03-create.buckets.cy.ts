41c2775af6fdd827ba5aca8d31ac2ec589d6b15f
Joao Esperancinha <jofisaes@gmail.com>
2022-08-03 16:47:01 +0200

    Tests that all metrics work on the first 10 seconds

--- e2e/cypress/e2e/influxdb/03-create.buckets.cy.ts
+++ e2e/cypress/e2e/influxdb/03-create.buckets.cy.ts
@@ -1,26 +1,35 @@
 describe('Influx Create Scraper DB Tests', () => {
     it('should creaate scraper', () => {
         cy.signIn();
-        cy.wait(1000);
+        const waitStep = 1;
+        cy.wait(waitStep);
         cy.get('button[aria-label="Show sub menu"]').eq(0).click();
         cy.get('div[id="buckets"] > a').eq(0).click({force: true});
+        cy.wait(waitStep);
 
         cy.get('button[title="Click to create a bucket"]').eq(0).click();
         cy.get('input[name="name"]').eq(0).clear().type('mos-influxDB');
         cy.get('button[title="Create"').eq(0).click();
+        cy.wait(waitStep);
+
+        cy.get('button[title="Click to create a bucket"]').eq(0).click();
+        cy.get('input[name="name"]').eq(0).clear().type('mos-influxDB2');
+        cy.get('button[title="Create"').eq(0).click();
+        cy.wait(waitStep);
 
         cy.get('button[title="Click to create a bucket"').eq(0).click();
         cy.get('input[name="name"]').eq(0).clear().type('mos-gui');
         cy.get('button[title="Create"').eq(0).click();
+        cy.wait(waitStep);
 
         cy.get('button[title="Click to create a bucket"').eq(0).click();
-        cy.get('input[name="name"]').eq(0).clear().type('mos-okta');
+        cy.get('input[name="name"]').eq(0).clear().type('mos-rest');
         cy.get('button[title="Create"').eq(0).click();
+        cy.wait(waitStep);
 
         cy.get('button[title="Click to create a bucket"').eq(0).click();
         cy.get('input[name="name"]').eq(0).clear().type('mos-jwt');
         cy.get('button[title="Create"').eq(0).click();
-
-        cy.wait(2000);
+        cy.wait(waitStep);
     });
 });
--- e2e/cypress/e2e/influxdb/04-create.scraper.cy.ts
+++ e2e/cypress/e2e/influxdb/04-create.scraper.cy.ts
@@ -1,41 +1,37 @@
 describe('Influx Create Scraper DB Tests', () => {
+    const waitStep = 1000;
+
     it('should creaate scraper', () => {
         cy.signIn();
-        const waitStep = 2000;
-        cy.wait(waitStep);
-        cy.get('button[aria-label="Show sub menu"]').eq(0).click();
         cy.get('div[id="scrapers"] > a').eq(0).click({force: true});
-
-        cy.get('button[title="Create a new Scraper"]').eq(0).click();
-        cy.get('input[title="Name"]').eq(0).clear().type('Influx DB Scraper');
-        cy.get('input[title="Target URL"]').eq(0).clear().type('http://localhost:8086/metrics');
-        cy.get('button[data-testid="bucket-dropdown--button"]').eq(0).click()
-        cy.get('div').contains('mos-influxDB').eq(0).click()
-        cy.get('button[title="Create"').eq(0).click();
-        cy.wait(waitStep);
-
-        cy.get('button[title="Create a new Scraper"]').eq(0).click();
-        cy.get('input[title="Name"]').eq(0).clear().type('Moving Objects Okta REST Service');
-        cy.get('input[title="Target URL"]').eq(0).clear().type('http://192.168.0.11:8082/aggregator/actuator/prometheus');
-        cy.get('button[data-testid="bucket-dropdown--button"]').eq(0).click()
-        cy.get('div').contains('mos-okta').eq(0).click()
-        cy.get('button[title="Create"').eq(0).click();
-        cy.wait(waitStep);
-
-        cy.get('button[title="Create a new Scraper"]').eq(0).click();
-        cy.get('input[title="Name"]').eq(0).clear().type('Moving Objects JWT Backend REST Service');
-        cy.get('input[title="Target URL"]').eq(0).clear().type('http://192.168.0.17:8081/objects/actuator/prometheus');
-        cy.get('button[data-testid="bucket-dropdown--button"]').eq(0).click()
-        cy.get('div').contains('mos-jwt').eq(0).click()
-        cy.get('button[title="Create"').eq(0).click();
-        cy.wait(waitStep);
-
-        cy.get('button[title="Create a new Scraper"]').eq(0).click();
-        cy.get('input[title="Name"]').eq(0).clear().type('GUI Node Scraper');
-        cy.get('input[title="Target URL"]').eq(0).clear().type('http://192.168.0.15:4000/metrics');
-        cy.get('button[data-testid="bucket-dropdown--button"]').eq(0).click()
-        cy.get('div').contains('mos-gui').eq(0).click()
-        cy.get('button[title="Create"').eq(0).click();
+        addScraper('Influx DB Scraper', 'http://localhost:8086/metrics', 'mos-influxDB', 'go_info');
+        addScraper('Moving Objects JWT Backend REST Service', 'http://192.168.0.17:8081/objects/actuator/prometheus', 'mos-jwt', 'application_started_time_seconds');
+        addScraper('GUI Node Scraper', 'http://192.168.0.15:4000/metrics', 'mos-gui', 'app_version');
+        addScraper('Moving Objects Okta REST Service', 'http://192.168.0.11:8082/aggregator/actuator/prometheus', 'mos-rest', 'application_started_time_seconds');
+        addScraper('Influx DB Scraper 2', 'http://localhost:8086/metrics', 'mos-influxDB2', 'go_info');
         cy.wait(waitStep);
     });
+
+    function addScraper(name, url, bucket, testMetric) {
+        cy.get('button[title="Create a new Scraper"]').eq(0).click();
+        cy.get('input[title="Name"]').eq(0).clear().type(name);
+        cy.get('input[title="Target URL"]').eq(0).clear().type(url);
+        cy.get('button[data-testid="bucket-dropdown--button"]').eq(0).click()
+        cy.get('div').contains(bucket).eq(0).click()
+        cy.get('button[title="Create"').eq(0).click();
+        performSwitch(bucket, testMetric)
+    }
+
+    function performSwitch(bucket, testMetric) {
+        cy.wait(waitStep);
+        cy.get('a[data-testid="nav-item-data-explorer"]').eq(0).click({force: true});
+        cy.wait(10000);
+        cy.reload()
+        cy.get('div').contains(bucket).click()
+        cy.get('div[data-testid="list--contents"]').children().should('not.be.empty')
+        cy.get('div').contains(testMetric).should('exist')
+        cy.wait(waitStep);
+        cy.get('div[id="scrapers"] > a').eq(0).click({force: true});
+    }
 });
+
