559ab5c834a43e14e3b7c6e0654cf4c0e4239987
Joao Esperancinha <jofisaes@gmail.com>
2022-08-03 22:47:20 +0200

    Finding the failing line Grafana

--- e2e/cypress/e2e/2-grafana/grafana.cy.ts
+++ e2e/cypress/e2e/2-grafana/grafana.cy.ts
@@ -4,21 +4,33 @@ describe('Grafana Tests', () => {
 
     it('show grafana', () => {
         cy.visit(`http://${host}:${port}`);
+        cy.log('1')
         cy.get('h1').contains("Grafana").should('exist');
+        cy.log('1')
         cy.get('input[aria-label="Username input field"]').type('admin');
+        cy.log('1')
         cy.get('input[aria-label="Password input field"]').type('admin');
+        cy.log('1')
         cy.get('button[aria-label="Login button"]').click();
+        cy.log('1')
         cy.get('input[name="newPassword"]').then($body => {
             cy.get('input[name="newPassword"]').type('admin');
             cy.get('input[name="confirmNew"]').type('admin');
         });
         cy.get('button[type = "submit"]').click();
+        cy.log('1')
         cy.reload()
+        cy.log('1')
         cy.get('a[aria-label="Dashboards"]').click();
+        cy.log('1')
         cy.wait(500);
+        cy.log('1')
         cy.get('div').contains(/(.*)Java(.*)/).should('exist');
+        cy.log('1')
         cy.get('div').contains(/(.*)Node(.*)/).should('exist');
+        cy.log('1')
         cy.get('div').contains(/(.*)Java(.*)/).click()
+        cy.log('1')
         cy.wait(200);
         cy.wait(200);
         cy.wait(200);
@@ -26,12 +38,14 @@ describe('Grafana Tests', () => {
         cy.wait(200);
         cy.wait(200);
         cy.get('a[aria-label="Dashboards"]').click();
+        cy.log('1')
         cy.wait(200);
         cy.wait(200);
         cy.wait(200);
         cy.wait(200);
         cy.wait(200);
         cy.wait(200);
+        cy.log('1')
         cy.get('div').contains(/(.*)Node(.*)/).click()
         cy.wait(200);
         cy.wait(200);
