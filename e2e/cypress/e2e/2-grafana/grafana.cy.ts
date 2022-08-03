cb91acd5b91e8010efeca7e352c6328b2b7da7bc
Joao Esperancinha <jofisaes@gmail.com>
2022-08-03 23:55:03 +0200

    Another try at Grafana

--- e2e/cypress/e2e/2-grafana/grafana.cy.ts
+++ e2e/cypress/e2e/2-grafana/grafana.cy.ts
@@ -2,57 +2,51 @@ describe('Grafana Tests', () => {
     const host = Cypress.env('host') ? 'grafana' : 'localhost';
     const port = 3000
 
+    const smallWait = 200;
+
     it('show grafana', () => {
         cy.visit(`http://${host}:${port}`);
-        cy.log('1')
         cy.get('h1').contains("Grafana").should('exist');
-        cy.log('1')
         cy.get('input[aria-label="Username input field"]').type('admin');
-        cy.log('1')
         cy.get('input[aria-label="Password input field"]').type('admin');
-        cy.log('1')
         cy.get('button[aria-label="Login button"]').click();
-        cy.log('1')
         cy.get('input[name="newPassword"]').then($body => {
             cy.get('input[name="newPassword"]').type('admin');
             cy.get('input[name="confirmNew"]').type('admin');
         });
         cy.get('button[type = "submit"]').click();
-        cy.log('1')
         cy.reload()
-        cy.log('1')
-        cy.get('a[aria-label="Dashboards"]').click();
-        cy.log('1')
-        cy.wait(500);
-        cy.log('1')
+        cy.get('a[aria-label="Dashboards"]').click({force: true});
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.reload()
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
         cy.get('div').contains(/(.*)Java(.*)/).should('exist');
-        cy.log('1')
         cy.get('div').contains(/(.*)Node(.*)/).should('exist');
-        cy.log('1')
         cy.get('div').contains(/(.*)Java(.*)/).click()
-        cy.log('1')
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
         cy.get('a[aria-label="Dashboards"]').click();
-        cy.log('1')
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
-        cy.log('1')
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
         cy.get('div').contains(/(.*)Node(.*)/).click()
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
-        cy.wait(200);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
+        cy.wait(smallWait);
     });
 
 })
