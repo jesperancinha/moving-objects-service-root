52d647dcdd35ad1b3194a1f887ac9e6d120a422b
Joao Esperancinha <jofisaes@gmail.com>
2022-08-17 13:14:30 +0200

    Sub Menu Unified

--- e2e/cypress/e2e/1-swagger/swagger.jwt.cy.ts
+++ e2e/cypress/e2e/1-swagger/swagger.jwt.cy.ts
@@ -1,13 +1,13 @@
 describe('Swagger Tests', () => {
-  const host = Cypress.env('host') ? Cypress.env('host') : 'localhost';
-  const port = Cypress.env('port') ? Cypress.env('port') : '8081';
+    const host = Cypress.env('host') ? Cypress.env('host') : 'localhost';
+    const port = Cypress.env('port') ? Cypress.env('port') : '8081';
 
-  it('shows swagger', () => {
-    cy.visit(`http://${host}:${port}/objects/webjars/swagger-ui/index.html`);
-    cy.get('h2', {timeout:10000}).contains('OpenAPI definition', {timeout: 10000}).should('not.be.null');
-    cy.wait(1000);
+    it('shows swagger', () => {
+        cy.visit(`http://${host}:${port}/objects/webjars/swagger-ui/index.html`);
+        cy.get('h2', {timeout: 10000}).contains('OpenAPI definition', {timeout: 10000}).should('not.be.null');
+        cy.wait(1000);
 
-    cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:8080/objects')
-  });
+        cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:8080/objects')
+    });
 
 })
--- e2e/cypress/e2e/4-influxdb/02-influxdb.cy.ts
+++ e2e/cypress/e2e/4-influxdb/02-influxdb.cy.ts
@@ -1,24 +1,28 @@
 describe('Influx Generic DB Tests', () => {
-    it ('all menus check', () => {
+    function clickSubMenu() {
+        cy.get('button[aria-label="Show sub menu"]').eq(0, {timeout: 10000}).click();
+    }
+
+    it('all menus check', () => {
         cy.signIn();
         cy.wait(1000);
-        cy.get('button[aria-label="Show sub menu"]').eq(0).click();
+        clickSubMenu();
         cy.reload();
         cy.get('div[id="sources"] > a').eq(0).click({force: true});
         cy.reload();
-        cy.get('button[aria-label="Show sub menu"]').eq(0).click();
+        clickSubMenu();
         cy.reload();
         cy.get('div[id="buckets"] > a').eq(0).click({force: true});
         cy.reload();
-        cy.get('button[aria-label="Show sub menu"]').eq(0).click();
+        clickSubMenu();
         cy.reload();
         cy.get('div[id="telegrafs"] > a').eq(0).click({force: true});
         cy.reload();
-        cy.get('button[aria-label="Show sub menu"]').eq(0).click();
+        clickSubMenu();
         cy.reload();
         cy.get('div[id="scrapers"] > a').eq(0).click({force: true});
         cy.reload();
-        cy.get('button[aria-label="Show sub menu"]').eq(0).click();
+        clickSubMenu();
         cy.reload();
         cy.get('div[id="tokens"] > a').eq(0).click({force: true});
     });
