4cbcb569d1a185adb4377f411283b655a9295fb4
João Filipe Sabino Esperancinha <jofisaes@hotmail.com>
2022-08-22 02:45:16 +0000

    Update dependency org.springdoc:springdoc-openapi-webflux-ui to v1.6.11

--- moving-objects-jwt-service/build.gradle.kts
+++ moving-objects-jwt-service/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.10")
+    implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.11")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 }
