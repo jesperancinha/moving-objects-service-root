69d83f63c2a13d77dda7015d44043d9f31e9a7be
Joao Esperancinha <jofisaes@gmail.com>
2022-08-27 11:19:11 +0200

    Code improvements

--- moving-objects-gui/entrypoint.sh
+++ moving-objects-gui/entrypoint.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+
+function replaceIp() {
+    service=$1
+    ip=$(getent hosts "$service" | awk '{ print $1 }')
+    sed -i 's/'"$service"'/'"$ip"'/g' /etc/nginx/conf.d/default.conf
+}
+
+replaceIp nginx
+replaceIp moving-objects-rest-service
+replaceIp moving-objects-jwt-service
+
+cat /etc/nginx/conf.d/default.conf
+
 pm2 start metrics-server/src/server.js
 nginx
 tail -f /dev/null
