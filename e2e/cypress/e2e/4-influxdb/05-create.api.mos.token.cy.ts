d9bb7c2539db77a4930ea30faa8000d7664ef002
Joao Esperancinha <jofisaes@gmail.com>
2022-08-28 09:52:42 +0200

    Runs detached telegraf container

--- e2e/cypress/e2e/4-influxdb/05-create.api.mos.token.cy.ts
+++ e2e/cypress/e2e/4-influxdb/05-create.api.mos.token.cy.ts
@@ -1,5 +1,5 @@
 describe('Influx Create API Token Tests', () => {
-    const waitStep = 1000;
+    const LONG_WAIT = 10000;
 
     it('should create API token for mos bucket', () => {
         cy.signIn();
@@ -9,7 +9,7 @@ describe('Influx Create API Token Tests', () => {
         cy.get('input[placeholder="Describe this new API Token"]').type("Mos Bucket Full Access API Token");
         cy.get('button[title="mos"').click({multiple: true});
         cy.get('span').contains('Save').click();
-        cy.get('span').contains('Mos Bucket Full Access API Token').eq(0).click();
+        cy.get('span').contains('Mos Bucket Full Access API Token', {timeout: LONG_WAIT}).eq(0).click();
         cy.get('code').then(control => {
             cy.writeFile("../docker-files/telegraf/token", control.html());
             cy.readFile("../.env").then(str => {
