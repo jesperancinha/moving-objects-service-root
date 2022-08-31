c5ed05bc236683cf6d7df79ac44c08e6cdd58554
Joao Esperancinha <jofisaes@gmail.com>
2022-08-31 18:50:32 +0200

    Redirect logout

--- moving-objects-gui/src/environments/environment.prod.ts
+++ moving-objects-gui/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 import {oktaConfig} from "./okta.config";
 
-const {CLIENT_ID, ISSUER, REDIRECT_URL, CLIENT_SECRET} = oktaConfig;
+const {CLIENT_ID, ISSUER, REDIRECT_LOGOUT, REDIRECT_URL, CLIENT_SECRET} = oktaConfig;
 
 export const environment = {
     CLIENT_ID: `${CLIENT_ID}`,
     CLIENT_SECRET: `${CLIENT_SECRET}`,
     ISSUER: `${ISSUER}`,
-    REDIRECT_LOGOUT: "http://localhost:4200/signout/",
+    REDIRECT_LOGOUT: `${REDIRECT_LOGOUT}`,
     REDIRECT_URL: `${REDIRECT_URL}`,
     environment: "prod",
     production: true,
