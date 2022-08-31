d2f1fe388cd4fee2c7f78a833dcee767634a6cc7
Joao Esperancinha <jofisaes@gmail.com>
2022-08-31 18:43:41 +0200

    Updates export vars

--- moving-objects-rest-service/exportVars.sh
+++ moving-objects-rest-service/exportVars.sh
@@ -10,26 +10,38 @@ echo 'Log into your https://developer.okta.com/ Dev account  and fill in the fol
 echo 'Be sure to have your user available with a user/password combination in Okta'
 echo 'At the end of this form, export command manually in the command line in order to start the start-demo-secure script'
 echo '- Set your ISSUER (https://...)'
+if [ -n "$ISSUER" ]; then
+    echo "(""$ISSUER"" as default)"
+fi
 printf "$ >"
 read -r issuer
-export ISSUER=$issuer
+export ISSUER=${issuer:-${ISSUER}}
 echo '- Set your CLIENT_ID'
+if [ -n "$CLIENT_ID" ]; then
+    echo "(""$CLIENT_ID"" as default)"
+fi
 printf "$ >"
 read -r clientId
-export CLIENT_ID=$clientId
+export CLIENT_ID=${clientId:-${CLIENT_ID}}
 echo '- Set your CLIENT_SECRET'
+if [ -n "$CLIENT_SECRET" ]; then
+    echo "(""$CLIENT_SECRET"" as default)"
+fi
 printf "$ >"
 read -r clientSecret
-export CLIENT_SECRET=$clientSecret
+export CLIENT_SECRET=${clientSecret:-${CLIENT_SECRET}}
+export REDIRECT_PORT=${redirectPort:-${REDIRECT_PORT:-8080}}
 echo '- Set your REDIRECT_PORT (the REDIRECT_URL is always localhost:REDIRECT_PORT for this project)'
-echo '- (8080 as default)'
+if [ -n "$REDIRECT_PORT" ]; then
+    echo "(""$REDIRECT_PORT"" as default)"
+fi
 printf "$ >"
 read -r redirectPort
 export REDIRECT_PORT=${redirectPort:-8080}
 echo -e '\033[1;34mExport data is:\033[0m'
-echo -e "* ISSUER=\033[1;34m$issuer\033[0m"
-echo -e "* CLIENT_ID=\033[1;34m$clientId\033[0m"
-echo -e "* CLIENT_SECRET=\033[1;34m$clientSecret\033[0m"
+echo -e "* ISSUER=\033[1;34m${ISSUER}\033[0m"
+echo -e "* CLIENT_ID=\033[1;34m${CLIENT_ID}\033[0m"
+echo -e "* CLIENT_SECRET=\033[1;34m${CLIENT_SECRET}\033[0m"
 echo -e "* REDIRECT_PORT=\033[1;34m${REDIRECT_PORT}\033[0m"
-echo -e "\033[1;32mexport ISSUER=$issuer CLIENT_ID=$clientId CLIENT_SECRET=$clientSecret REDIRECT_PORT=${REDIRECT_PORT}"
+echo -e "\033[1;32mexport ISSUER=${ISSUER} CLIENT_ID=${CLIENT_ID} CLIENT_SECRET=${CLIENT_SECRET} REDIRECT_PORT=${REDIRECT_PORT}"
 echo -e '\033[1;32mDone!\033[0m'
