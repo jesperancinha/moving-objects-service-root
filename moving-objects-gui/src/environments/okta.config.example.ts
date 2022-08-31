f7b32b5c72ad8de48a4de8046b219a02676d8622
Joao Esperancinha <jofisaes@gmail.com>
2022-08-31 18:52:59 +0200

    Example demo

--- moving-objects-gui/src/environments/okta.config.example.ts
+++ moving-objects-gui/src/environments/okta.config.example.ts
@@ -2,5 +2,6 @@ export const oktaConfig = {
     CLIENT_ID: "AAAAAAAAAAAAAAAAAAAA",
     CLIENT_SECRET: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
     ISSUER: "https://dev-DEVID.okta.com/",
-    REDIRECT_URL: "http://localhost:4200/login/callback",
+    REDIRECT_LOGOUT: "http://localhost:8080/signout",
+    REDIRECT_URL: "http://localhost:8080/callback",
 };
