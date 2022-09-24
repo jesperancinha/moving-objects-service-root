a5092ab1652178f72c85a34456730999cbde83c1
Joao Esperancinha <jofisaes@gmail.com>
2022-09-24 09:51:38 +0200

    Removes IP dependent configuration setup

--- moving-objects-gui/entrypoint.sh
+++ moving-objects-gui/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-function replaceIp() {
-    service=$1
-    ip=$(getent hosts "$service" | awk '{ print $1 }')
-    sed -i 's/'"$service"'/'"$ip"'/g' /etc/nginx/conf.d/default.conf
-}
-
-replaceIp moving-objects-rest-service
-replaceIp moving-objects-jwt-service
+#IP replacement has been removed because with fixed IP's docker-compose may disrupt local network
+#If you are running an old p project and you run into network issues, please update your version, remove all docker networks and start again.
+#function replaceIp() {
+#    service=$1
+#    ip=$(getent hosts "$service" | awk '{ print $1 }')
+#    sed -i 's/'"$service"'/'"$ip"'/g' /etc/nginx/conf.d/default.conf
+#}
+#
+#replaceIp moving-objects-rest-service
+#replaceIp moving-objects-jwt-service
 
 cat /etc/nginx/conf.d/default.conf
 
