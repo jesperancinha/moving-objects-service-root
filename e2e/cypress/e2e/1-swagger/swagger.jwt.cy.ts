2ea24f79e3ea8119e55c40f2d4995dd333f32a76
João Esperancinha <jofisaes@gmail.com>
2025-09-08 21:06:18 +0200

    Type on the first

--- e2e/cypress/e2e/1-swagger/swagger.jwt.cy.ts
+++ e2e/cypress/e2e/1-swagger/swagger.jwt.cy.ts
@@ -5,7 +5,7 @@ describe('Swagger Tests', () => {
     it('shows swagger', () => {
         cy.visit(`http://${host}:${port}/objects/swagger-ui.html`);
         cy.get("input").clear();
-        cy.get("input").type("/objects/v3/api-docs{enter}");
+        cy.get("input").eq(0).type("/objects/v3/api-docs{enter}");
         cy.get('h2', {timeout: 10000}).contains('OpenAPI definition', {timeout: 10000}).should('not.be.null');
         cy.wait(1000);
 
